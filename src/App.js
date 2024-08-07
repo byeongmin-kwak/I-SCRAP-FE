1ca53fde418ee88cfbc4ced531a6764a7f35dd78
byeongmin-kwak <bqudmals@naver.com>
2024-08-07 16:47:18 +0900

    상단바 제작

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage/MainPage";
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Nav.css";
+import logo from "../../assets/logo.png";
+
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <ul className="navbar-menu">
+        <li>지도</li>
+        <li>찾기</li>
+        <li>기록</li>
+        <li>My</li>
+        <li className="login-button">login / join</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
