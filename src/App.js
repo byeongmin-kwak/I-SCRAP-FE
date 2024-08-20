5265534afa89e9ad0330102a436e7e2add1366ad
byeongmin-kwak <bqudmals@naver.com>
2024-08-20 19:36:43 +0900

    MapPage 디자인

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Nav from "./components/Nav/Nav";
 import NonLoginMainPage from "./pages/NonLoginMainPage/NonLoginMainPage";
 import LoginMainPage from "./pages/LoginMainPage/LoginMainPage";
+import MapPage from "./pages/MapPage/MapPage";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -17,6 +18,7 @@ function App() {
         ) : (
           <Route path="/" element={<NonLoginMainPage />} />
         )}
+        <Route path="/map" element={<MapPage />} />
       </Routes>
     </div>
   );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,13 +3,23 @@ import styles from "./Nav.module.css";
 import logo from "../../assets/logo.svg";
 
 const Nav = () => {
+  const handleLogoClick = () => {
+    window.location.href = "/"; // 로고 클릭 시 새로고침과 함께 메인 페이지로 이동
+  };
+
+  const handleMapClick = () => {
+    window.location.href = "/map"; // "지도" 클릭 시 새로고침과 함께 /map 페이지로 이동
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarLogo}>
-        <img src={logo} alt="Logo" />
+        <img src={logo} alt="Logo" onClick={handleLogoClick} />
       </div>
       <ul className={styles.navbarMenu}>
-        <li>지도</li>
+        <li>
+          <li onClick={handleMapClick}>지도</li>
+        </li>
         <li>찾기</li>
         <li>기록</li>
         <li>My</li>
