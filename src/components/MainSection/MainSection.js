d416caaed469fbf868de24e5b37f0a7243314590
byeongmin-kwak <bqudmals@naver.com>
2024-10-03 18:24:02 +0900

    취향별 테스트 이동 추가

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -7,8 +7,9 @@ import image1 from "../../assets/MainPage/MainSectionImage1.svg";
 import image2 from "../../assets/MainPage/MainSectionImage2.png";
 import image3 from "../../assets/MainPage/MainSectionImage3.svg";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const screens = (character, isCharacterLoaded) => [
+const screens = (character, isCharacterLoaded, navigate) => [
   {
     id: 0,
     component: (
@@ -33,6 +34,10 @@ const screens = (character, isCharacterLoaded) => [
     component: (
       <div className={styles.screen}>
         <img src={image2} alt="Main" className={styles.image2} />
+        <div
+          className={styles.clickableArea}
+          onClick={() => navigate("/test")}
+        ></div>
       </div>
     ),
   },
@@ -52,6 +57,7 @@ const MainSection = () => {
     "https://iscrap-image.s3.ap-northeast-2.amazonaws.com/icecreamCharacter/vanilla.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAQMEY6GDSFD55X2WN%2F20241003%2Fap-northeast-2%2Fs3%2Faws4_request&X-Amz-Date=20241003T052305Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEG0aDmFwLW5vcnRoZWFzdC0yIkcwRQIgWZQoHZnQ%2Bv5AJyoNHmGtTEk%2Bxm8bR4H00Q2MTgU5dR8CIQD6nXjw70SMDBrckfDu9h2u1CMeC6dYTR73cnY1HmdhJCqAAwi2%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAAaDDAyNjA5MDU0OTQ3NiIMYX366pkdctUDHpZWKtQC6%2F9WJnwKDV7vnD1JE3tUuH3gyXBilaw7NEVhwJdcYZbejeierTpQ%2B83rysj1vThXjJUHdEItDGjL%2FxUMfw0cH%2B1Cu%2BsmJ91%2F8YUua%2F4i0OfWRhA0%2FXHmDJdFMMKe50FUPWvCh3lF%2BNxvqyolDKb7bzqfUi37LqUqie9ZzoL0LoWz5k7LSE4Z4LPzQ4D0Yq7%2BbVNuxkXku%2Bn4Wj9g5bXWYz7xoe6%2FpoRtbMRF6DkER3RfGCaLXbc97IysBgpAAj%2FRnCEJRT0FC3gKQ2G4VmPDxlllNlYRTiVAgQ6qdlBIj5%2F0S6FuNj2%2FvR0iyHaYRb8LNPw%2FEAYVyHs6B5PiG9FePn%2F18QnAANl%2BqHpeNlcTYZyNWhiw7PgkNFGUw8ZCzf65Vbt4kbpfuHgL7kKdG8mXeehXMe5uMBCvqID2PudJu1afJ2y0Wvp%2F2yIj%2Bs85%2B4pb9OULjDDByvi3BjqeAe7ozedBSkaO9qt%2FiQHVIFTMy6oQiqLAQ7brMBeILYGlLVp2ARzuCJWwPR6Zp%2BScTcziO0odd3hpkJ3t6zRa8JSpdZgWJoMQTqRiRT9qoR%2BAUu1YxyPTT9YzSKNvZksYyGuJO7mIYQTMF2ISUiOdunAgfFOGt%2FL7H9V2lrrot%2FtjA62PGMpwr1AcGK9xLd%2BEmqjRvowmQavMA%2BWzucOp&X-Amz-Signature=28830b4751069007e9cde104c1c8d6beb8d4e114c94e7cb2ffe0f13d4aa7cca3&X-Amz-SignedHeaders=host&x-id=GetObject"
   );
   const [isCharacterLoaded, setIsCharacterLoaded] = useState(false); // 로딩 상태 추가
+  const navigate = useNavigate();
 
   const serverURL = process.env.REACT_APP_SERVER_URL;
 
@@ -110,11 +116,13 @@ const MainSection = () => {
           transform: `translateX(${currentScreen * 100}vw)`,
         }}
       >
-        {screens(character, isCharacterLoaded).map((screen, index) => (
-          <div className={styles.screenContainer} key={index}>
-            {screen.component}
-          </div>
-        ))}
+        {screens(character, isCharacterLoaded, navigate).map(
+          (screen, index) => (
+            <div className={styles.screenContainer} key={index}>
+              {screen.component}
+            </div>
+          )
+        )}
       </div>
 
       {/* 이미지가 로드되면 handleImageLoad 호출 */}
